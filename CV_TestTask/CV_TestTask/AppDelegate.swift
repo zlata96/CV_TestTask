aa7b98c818b3b599985a355671d00985dc9a5201
Zlata Guseva <gusevazlata96@gmail.com>
2023-08-01 11:51:00 +0400

    Initial commit

---
+++ CV_TestTask/CV_TestTask/AppDelegate.swift
@@ -0,0 +1,25 @@
+// AppDelegate.swift
+// CV_TestTask. Created by Zlata Guseva.
+
+import UIKit
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(
+        _: UIApplication,
+        didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        setupRootViewController()
+        return true
+    }
+
+    private func setupRootViewController() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ CV_TestTask/CV_TestTask/ViewController.swift
@@ -0,0 +1,10 @@
+// ViewController.swift
+// CV_TestTask. Created by Zlata Guseva.
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
