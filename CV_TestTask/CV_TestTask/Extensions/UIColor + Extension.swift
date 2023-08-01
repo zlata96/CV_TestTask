42b1c4c1e656ba21408f1f428fb21d3fef400f4c
Zlata Guseva <gusevazlata96@gmail.com>
2023-08-01 13:39:09 +0400

    Add new color asset

---
+++ CV_TestTask/CV_TestTask/Extensions/UIColor + Extension.swift
@@ -0,0 +1,10 @@
+// UIColor + Extension.swift
+// CV_TestTask. Created by Zlata Guseva.
+
+import UIKit
+
+extension UIColor {
+    static let subTitle = UIColor(named: "subTitle")
+    static let mainTitle = UIColor(named: "mainTitle")
+    static let mainBackground = UIColor(named: "mainBackground")
+}
